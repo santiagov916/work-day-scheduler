d1d867ca282168e9f5bf68a6cef2b420f2ddcfe0
santiago <72000936+santiagov916@users.noreply.github.com>
2021-11-07 16:51:56 -0800

    add color to the hour position

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,28 @@ $(document).ready(function () {
             localStorage.setItem(time, value)
         
         });
+        //get current hour
+        var hour = moment().format("k");
+        for(let i = 9; i < 18; i++) {
+            let saved = localStorage.getItem("hour-" + i);
+
+            if (saved) {
+                $(".hour-" + i).val(saved);
+            }
+
+            //color for each hour position in time
+
+            // if hour is = to i then add the "present" class to the hour- variable with the matching "i"
+            if (hour == i) {
+                $(".hour-" + i).addClass("present");
+            }
+            // if hour is less than i then add the "future" class to the hour- variable with the matching "i"
+            if (hour < i) {
+                $(".hour-" + i).addClass("future");
+            }
+            // if hour is greater than i then add the "past" class to the hour- variable with the matching "i"
+            if (hour > i) {
+                $(".hour-" + i).addClass("past");
+            }
+        }
     });
